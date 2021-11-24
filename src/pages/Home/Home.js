299b0be0fdd37a7b32b6499d179371529eb3bcd3
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2021-11-24 21:09:14 +0200

    Complete Home Page Component Starter
    - Added UI components based on the figma design
    - Restyled button to match template also

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,21 @@ import './Home.css';
 
 function Home () {
 	return(
-		<h1>Test Home Page</h1>
+		<>
+			<div class="home-page-container">
+			<h1 class="home-header-text">Upload Your Image</h1>
+			<p class="home-header-subtext">File should be Jpeg, png.......</p>
+			<div class="image-upload-container">
+				<div class="image-upload-vector-container">
+				</div>
+				<p class="image-upload-text"> Drag and Drop your image here</p>
+			</div>
+			<p class="image-upload-text-OR"> OR </p>
+			<div>
+			<button class="image-upload-button"> Choose a File! </button>
+			</div>
+			</div>
+		</>
 	);
 }
 
