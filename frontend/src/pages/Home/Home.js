ba13677ac5976ca9025d7b2ee44b217dc04757d5
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2021-11-30 14:02:23 +0200

    Change class to classNames for Web Semantics

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,23 +1,42 @@
+import { Component } from 'react';
 import './Home.css';
 
-function Home () {
-	return(
-		<>
-			<div class="home-page-container">
-			<h1 class="home-header-text">Upload Your Image</h1>
-			<p class="home-header-subtext">File should be Jpeg, png.......</p>
-			<div class="image-upload-container">
-				<div class="image-upload-vector-container">
+class Home extends Component {
+	constructor(props){
+		super(props);
+		// this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit = (event) => {
+		console.log(3);
+		// event.preventDefault();
+	}
+
+	onChangeFileInput = (selectedFiles) => {
+		console.log(selectedFiles);
+		this.handleSubmit();
+	}
+	render() {
+		return(
+			<>
+				<div className="home-page-container">
+				<h1 className="home-header-text">Upload Your Image</h1>
+				<p className="home-header-subtext">File should be Jpeg, png.......</p>
+				<div className="image-upload-container">
+					<div className="image-upload-vector-container">
+					</div>
+					<p className="image-upload-text"> Drag and Drop your image here</p>
 				</div>
-				<p class="image-upload-text"> Drag and Drop your image here</p>
-			</div>
-			<p class="image-upload-text-OR"> OR </p>
-			<div>
-			<button class="image-upload-button"> Choose a File! </button>
-			</div>
-			</div>
-		</>
-	);
+				<p className="image-upload-text-OR"> OR </p>
+				<div>
+					<form id="form" onSubmit={this.props.handleSubmit}>
+							<input id="files" type="file" className="image-upload-button" accept="image/*"onChange={(e) => this.onChangeFileInput(e.target.files)}/>	 
+					</form>
+				</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default Home;
--- frontend/src/pages/SuccessfulUpload/SuccessfulUpload.js
+++ frontend/src/pages/SuccessfulUpload/SuccessfulUpload.js
@@ -3,13 +3,13 @@ import PlaceHolderImage from '../../images/338x225-placeholder-image.png'
 
 function SuccesfulUpload() {
 	return(
-		<div class="succesful-upload-main-container">
-			<div class="green-tick-vector-image-container"></div>
-			<p class="successful-upload-text"> Uploaded Successfully! </p>
-			<img src={PlaceHolderImage} class="uploaded-image"/>
-			<div class="upload-copy-link-container">
-				<p class="upload-image-link">https://sampleLink/onyourdomain/imagesucessfulyuploaded.com</p>
-				<button class="upload-image-link-copy-button"> Copy Link</button>
+		<div className="succesful-upload-main-container">
+			<div className="green-tick-vector-image-container"></div>
+			<p classNameName="successful-upload-text"> Uploaded Successfully! </p>
+			<img src={PlaceHolderImage} className="uploaded-image"/>
+			<div className="upload-copy-link-container">
+				<p className="upload-image-link">https://sampleLink/onyourdomain/imagesucessfulyuploaded.com</p>
+				<button className="upload-image-link-copy-button"> Copy Link</button>
 			</div>
 		</div>
 	);
