e1585bd7319d09e0e614e276532cd67019fd07f6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2021-12-25 11:26:39 +0200

    Require connection to database when starting server

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const express = require('express'),
       multer = require('multer'),
 	  mongoose = require('mongoose'),
 	  uuidv4 = require('uuid');
+		database = require('./database/database.js');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+database.connectToDatabase();
 app.get('/', (req, res) => res.send('Upload Images API is running !'))
 app.listen(3000, () => console.log('Server ready'))
 app.use('/api/images', images)
