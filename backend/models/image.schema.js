7a6d1b04005c681ce193aa21a23276e007805c61
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-07 14:07:56 +0200

    Remove backend from this repository to be placed in another

--- backend/models/image.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const imageSchema = new Schema({
-	cloudinaryURL: String,
-	dateUploaded: String,
-});
-
-module.exports = new mongoose.model('Image', imageSchema);
